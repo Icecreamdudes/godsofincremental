0fe0c7ec4c2b685993ac76b4170f103c04d638f0
Icecreamdude <72051053+Icecreamdudes@users.noreply.github.com>
2023-09-01 11:55:00 -0400

    link

--- js/technical/systemComponents.js
+++ js/technical/systemComponents.js
@@ -143,6 +143,7 @@ var systemComponents = {
         <span v-if="modInfo.discordLink"><a class="link" v-bind:href="modInfo.discordLink" target="_blank">{{modInfo.discordName}}</a><br></span>
         <a class="link" href="https://discord.gg/F3xveHV" target="_blank" v-bind:style="modInfo.discordLink ? {'font-size': '16px'} : {}">The Modding Tree Discord</a><br>
         <a class="link" href="http://discord.gg/wwQfgPa" target="_blank" v-bind:style="{'font-size': '16px'}">Main Prestige Tree server</a><br>
+        <a class="link" href="https://www.youtube.com/watch?v=Rhj5ULjqRlg" target="_blank" v-bind:style="{'font-size': '16px'}">Soundtrack</a><br>
 		<br><br>
         Time Played: {{ formatTime(player.timePlayed) }}<br><br>
         <h3>Hotkeys</h3><br>
